4da880a73096b32b5a463cfdd8a603cffd91c29f
forgoroe <forgoroe@gmail.com>
2016-10-22 00:24:17 +0200

    add iframe preview and other

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# wikipedia-viewer
-Search for wikipedia entries or query a random page
+#Wikipedia-viewer
+
+Search for [wikipedia](https://www.wikipedia.org/) entries, get an embedded preview or query a random wikipedia page.
+
+Still a work in progress, but getting closer to completion. This small web app should be viewable both on desktop and mobile devices, hence the preview:
+
+![preview](http://res.cloudinary.com/forgoroe/image/upload/c_scale,w_1036/v1477087779/previews/wikipedia_viewer_preview.png)
+
+##Motivation
+
+This project was made by following the FreeCodeCamp user stories for the [wikipedia-viewer](https://www.freecodecamp.com/challenges/build-a-wikipedia-viewer) project and by implementing a few of my own other ideas.
+
+##Resources and APIs
+
+- [Bootstrap](http://getbootstrap.com/)
+- [jQuery](https://jquery.com/) (very little of it)
+- [Wikimedia API](https://www.mediawiki.org/wiki/API:Main_page)
--- js/script.js
+++ js/script.js
@@ -2,14 +2,34 @@ window.onload = function() {
     var basicUrlRequest = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=';
     var basicThumbnailRequest = 'https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&pilimit=10&pithumbsize=100&indexpageids=&format=json&titles=';
     var callback = '&callback=?';
+    var previousSearchWord = '';
     var searchWord;
 
-    window.capture = function capture(evt) {
+    setInterval(function() {
+        searchWord = document.getElementById('searchInput').value;
+        if (previousSearchWord == searchWord) {
+            previousSearchWord = searchWord;
+        } else {
+            search();
+            previousSearchWord = searchWord;
+        }
+    }, 2500);
 
+    document.getElementById('searchInput').onkeydown = function(e) {
+        if (e.keyCode == 13) {
+            return false;
+        }
     };
 
     document.getElementById('searchButton').onclick = function() {
+        search();
+    }
+
+    function search() {
         searchWord = document.getElementById('searchInput').value;
+        if (searchWord == '') {
+            removePreviousResults();
+        }
         fetchData(searchWord);
     }
 
@@ -58,8 +78,6 @@ window.onload = function() {
         displayResults(collectionData);
     }
 
-
-
     function displayResults(dataCollection) {
 
         var resultList = document.getElementById("resultList");
@@ -80,6 +98,7 @@ window.onload = function() {
             var anchorElement = document.createElement("a");
             var h2Element = document.createElement("h2");
             var pElement = document.createElement("p");
+            var p2Element = document.createElement("p");
 
             var titleNode = document.createTextNode(titleData);
             var infoNode = document.createTextNode(infoData);
@@ -89,24 +108,48 @@ window.onload = function() {
 
             h2Element.className = 'list-group-item-heading';
             pElement.className = 'list-group-item-text';
+            p2Element.className = 'list-group-item-text';
+            p2Element.style = 'margin-top:6px; margin-bottom:6px;';
             h2Element.innerHTML = '<span style="padding-right:3px; padding-top: 3px; display:inline-block;">' +
                 '<img src="' + thumbnailSource + '"></span>';
+            p2Element.innerHTML = '<button type="button" class="btn btn-default btn-sm">' +
+                '<span class="glyphicon glyphicon-eye-open"></span> Preview </button>';
+
             h2Element.appendChild(titleNode);
             anchorElement.appendChild(h2Element);
 
+            pElement.appendChild(p2Element);
             pElement.appendChild(infoNode);
+
 
             liElement.appendChild(anchorElement);
             liElement.appendChild(pElement);
-
             resultList.appendChild(liElement);
+
+            //this closure is kinda awesome. I'm so smart (after 50 tries)
+            document.getElementsByClassName('btn btn-default btn-sm')[i].onclick = (function() {
+                var currentSrc = dataCollection.pageURLData[i];
+                return function() {
+                    var iframe = document.getElementsByTagName('iframe')[0];
+                    iframe.removeAttribute('hidden');
+                    iframe.src = currentSrc;
+                }
+            }(dataCollection.pageURLData[i]));
+
+
         }
     }
+
     function removePreviousResults(results) {
         if (resultList.hasChildNodes()) {
             while (resultList.firstChild) {
                 resultList.removeChild(resultList.firstChild);
             }
+        }
+        if (document.getElementsByTagName('iframe')[0]) {
+            var iframe = document.getElementsByTagName('iframe')[0];
+            iframe.src = '#';
+            iframe.setAttribute('hidden', true);
         }
     }
 
